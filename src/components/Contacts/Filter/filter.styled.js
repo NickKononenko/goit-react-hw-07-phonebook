2181d15b647cbcdc6874d7898f4fb4e46614fabe
NickKononenko <kononenko0812@gmail.com>
2024-02-15 18:17:58 +0200

    fixed

---
+++ src/components/Contacts/Filter/filter.styled.js
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled';
+
+export const FilterCOntainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+
+  padding: 10px 0;
+`;
+
+export const Title = styled.h3`
+  font-weight: 300;
+`;
+
+export const Input = styled.input`
+  width: 14rem;
+`;
